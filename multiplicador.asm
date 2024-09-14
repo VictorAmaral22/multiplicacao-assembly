8be8ce1eda5aa5a32ecaf40b53644c753f865181
Eric Corrêa <126991412+Kiincoder@users.noreply.github.com>
2024-09-13 21:32:13 -0300

    12123123

--- multiplicador.asm
+++ multiplicador.asm
@@ -1,29 +1,41 @@
 .data 
- num1: db #00h
- num2: db #04h
+ num1: db #08h
+ num2: db #00h
  cont: db #00h
- result: db #00h
+ resultado: db #00h
+ maior: db #00h
 .enddata
 
 INICIO:	
- lda num2
- jz FIM
-
  lda num1
  jz FIM
-
- add cont
- sta cont
-
- MULT_LOOP:
+ lda num2
+ jz FIM
+ not
+ add #01h
+ add num1
+ jn NUM1_MENOR
+ 
+ NUM2_MENOR:
   lda num2
-  add result
-  sta result
-
-  lda cont
-  add #0ffh
+  sta cont 
+  lda num1
+  sta maior
+  jmp MULT_LOOP
+  
+ NUM1_MENOR:
+  lda num1
   sta cont
-
+  lda num2
+  sta maior   
+ 
+ MULT_LOOP:
+  lda maior
+  add resultado
+  sta resultado
+  lda cont
+  add #0ffh	
+  sta cont
   jz FIM
   jmp MULT_LOOP
 
